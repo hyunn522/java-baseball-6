13c6eb4bc5c3aa78aab9dd10a3c4c6e1a7772281
hyunn522 <tjgus52213@gmail.com>
2024-10-14 21:23:37 +0900

    feat: 게임 시작 시 반환할 StartView 추가

---
+++ src/main/java/baseball/View/StartView.java
@@ -0,0 +1,10 @@
+package baseball.View;
+
+import static baseball.Model.enumMessages.UserMessage.START_MESSAGE;
+
+public class StartView {
+
+    public static void printStartView() {
+        System.out.println(START_MESSAGE.getMessage());
+    }
+}
