931b78aba0ef230e6329c98e13a52be615e31305
hyunn522 <tjgus52213@gmail.com>
2024-10-15 13:38:40 +0900

    refactor: Game 객체 내로 책임 위임

--- src/main/java/baseball/Model/Game.java
+++ src/main/java/baseball/Model/Game.java
@@ -1,5 +1,8 @@
 package baseball.Model;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
@@ -11,5 +14,25 @@ public class Game {
 
     public List<Integer> getGameNum() {
         return this.gameNum;
+    }
+
+    public static Game generateRandomGame() {
+        ArrayList<Integer> randomList = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            randomList.add(pickNumberInRange(1, 9));
+            for (int j = 0; j < i; j++) {
+                if (randomList.get(j) - randomList.get(i) == 0) {
+                    randomList.remove(i);
+                    i--;
+                }
+            }
+        }
+
+        for (int i : randomList) {
+            System.out.println(i);
+        }
+
+        return new Game(randomList);
     }
 }
--- src/main/java/baseball/Service/GameService.java
+++ src/main/java/baseball/Service/GameService.java
@@ -1,7 +1,5 @@
 package baseball.Service;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-
 import baseball.Model.Game;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,23 +8,7 @@ import java.util.List;
 public class GameService {
 
     public Game createGame() {
-        return new Game(generateRandomNum());
-    }
-
-    public static ArrayList<Integer> generateRandomNum() {
-        ArrayList<Integer> randomList = new ArrayList<>();
-
-        for (int i = 0; i < 3; i++) {
-            randomList.add(pickNumberInRange(1, 9));
-            for (int j = 0; j < i; j++) {
-                if (randomList.get(j) - randomList.get(i) == 0) {
-                    randomList.remove(i);
-                    i--;
-                }
-            }
-        }
-        
-        return randomList;
+        return Game.generateRandomGame();
     }
 
     public ArrayList<Integer> compareNums(Game user, Game answer) {
